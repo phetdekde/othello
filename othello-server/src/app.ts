abf0abc91c0c13e526797bf313e3c53cf25d6e39
Jirawat Santichavalit <phetdekde@hotmail.com>
2021-10-17 23:51:23 +0700

    .

--- othello-server/src/app.ts
+++ othello-server/src/app.ts
@@ -9,6 +9,7 @@ import "reflect-metadata";
 var indexRouter = require('./routes/index');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'routes'));
@@ -19,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 
