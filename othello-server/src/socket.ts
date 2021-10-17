fad495aeab5fa2b22ef400220f868946eec8aaee
Jirawat Santichavalit <phetdekde@hotmail.com>
2021-10-18 00:11:04 +0700

    .

--- othello-server/src/socket.ts
+++ othello-server/src/socket.ts
@@ -2,13 +2,15 @@ import { useSocketServer } from "socket-controllers";
 import { Server } from "socket.io";
 
 export default (httpServer) => {
-  const io = new Server(httpServer, {
+  const io = new Server({
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
       credentials: true
     },
   });
+
+  io.attach(httpServer)
 
   useSocketServer(io, { controllers: [__dirname + "/api/controllers/*.ts"] });
 
