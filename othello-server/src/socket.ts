435d3ea51418be7dfe05c8c2f4ae6e2471d9edc4
Jirawat Santichavalit <phetdekde@hotmail.com>
2021-10-18 01:17:04 +0700

    .

--- othello-server/src/socket.ts
+++ othello-server/src/socket.ts
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "*",
+      origin: "https://evening-cove-27499.herokuapp.com",
       methods: ["GET", "POST"],
       credentials: true,
     },
