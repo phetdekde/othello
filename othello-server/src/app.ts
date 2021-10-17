d1e3979ac87fd33fde1d5182190e81d667425824
Jirawat Santichavalit <phetdekde@hotmail.com>
2021-10-17 21:25:41 +0700

    .

--- othello-server/src/app.ts
+++ othello-server/src/app.ts
@@ -19,9 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'https://othello-react.netlify.app'
-}));
+app.use(cors());
 
 app.use('/', indexRouter);
 
--- othello-server/src/socket.ts
+++ othello-server/src/socket.ts
@@ -4,9 +4,7 @@ import { Server } from "socket.io";
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "https://othello-react.netlify.app",
-      methods: ["GET", "POST"],
-      credentials: true
+      origin: "*",
     },
   });
 
