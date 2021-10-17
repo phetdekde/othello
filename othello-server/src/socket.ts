7a4fa93fc2a21fc58aba6c36bff07ca1cb28c2bd
Jirawat Santichavalit <phetdekde@hotmail.com>
2021-10-17 22:50:49 +0700

    .

--- othello-server/src/socket.ts
+++ othello-server/src/socket.ts
@@ -4,9 +4,8 @@ import { Server } from "socket.io";
 export default (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: true,
+      origin: "https://othello-react.netlify.app",
       methods: ["GET", "POST"],
-      credentials: true
     },
   });
 
